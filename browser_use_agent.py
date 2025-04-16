1cbb187233f1cffc81eac2d363feda9442cc8c69
Vishnu Kadaba <vishnuswarupkadaba@gmail.com>
2025-04-16 11:08:00 -0700

    browser use agent barebones

---
+++ browser_use_agent.py
@@ -0,0 +1,21 @@
+from langchain_openai import ChatOpenAI
+from browser_use import Agent
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import asyncio
+
+llm = ChatOpenAI(model="gpt-4o")
+
+
+async def main():
+    agent = Agent(
+        task="open google",
+        llm=llm,
+    )
+    result = await agent.run()
+    print(result)
+
+
+asyncio.run(main())
